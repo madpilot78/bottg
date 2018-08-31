1b2a58e1f6cb1db39757e0f2d07dba752d872eeb
Guido Falsi <mad@madpilot.net>
2018-08-31 13:17:13 +0200

    Add stub for new test class.

---
+++ tests/BotTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace madpilot78\bottg\tests;
+
+use madpilot78\bottg\Bot;
+
+class BotTest extends \PHPUnit\Framework\TestCase
+{
+    use \phpmock\phpunit\PHPMock;
+
+    /**
+     * The object being tested lives here.
+     *
+     * @var $bot
+     */
+    private $bot;
+
+    /**
+     * Setup code.
+     *
+     * Instantiates class to be tested.
+     *
+     * @return void
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->bot = new Bot('token');
+    }
+
+    /**
+     * Teardown code.
+     *
+     * Frees tested object.
+     *
+     * @return void
+     */
+    protected function tearDown()
+    {
+        parent::tearDown();
+        $this->bot = null;
+    }
+
+    /**
+     * Test object is correctly insstantiated
+     *
+     * @return void
+     */
+    public function testCreatedObject()
+    {
+        $this->assertInstanceOf(Bot::class, $this->bot);
+    }
+}
