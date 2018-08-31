a03c990196e4dbeda643aecbc933c56d1a33a999
Guido Falsi <mad@madpilot.net>
2018-08-31 16:46:05 +0200

    Stub Bot class.

---
+++ src/Bot.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace madpilot78\bottg;
+
+class Bot
+{
+    /**
+     * The token being used
+     *
+     * @var
+     */
+    private $token;
+
+    /**
+     * Constructor, requires valid bot token
+     *
+     * @param   string $token
+     * @return  void
+     */
+    public function __construct(string $token)
+    {
+        $this->token = $token;
+    }
+}
