40f055d252965ab68d8ed767c1edb171d263e455
Guido Falsi <mad@madpilot.net>
2018-09-01 17:47:43 +0200

    Add basic test for forthcoming class.

---
+++ tests/API/RequestTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace madpilot78\bottg\tests\API;
+
+use Faker;
+use madpilot78\bottg\API\Request;
+
+class RequestTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * Provides the available types of requests.
+     *
+     * @return array
+     */
+    public function requestTypeProvider()
+    {
+        return [
+            [Request::GET],
+            [Request::SUBMIT],
+            [Request::MPART],
+            [Request::JSON]
+        ];
+    }
+
+    /**
+     * Test creating object without providing fields.
+     *
+     * @dataProvider requestTypeProvider
+     *
+     * @param  int $type
+     *
+     * @return void
+     */
+    public function testCanCreateRequestWithoutFields(int $type)
+    {
+        $api = 'test';
+
+        $req = new Request($type, $api);
+        $this->assertInstanceOf(Request::class, $req);
+        $this->assertEquals($type, $req->getType());
+        $this->assertEquals($api, $req->getAPI());
+        $this->assertNull($req->getFields());
+    }
+
+    
+    /**
+     * Test creating object providing fields.
+     *
+     * @dataProvider requestTypeProvider
+     *
+     * @param  int $type
+     *
+     * @return void
+     */
+    public function testCanCreateRequestWithFields(int $type)
+    {
+        $api = 'test';
+        $fields = [ 'foo' => 'bar' ];
+
+        $req = new Request($type, $api, $fields);
+        $this->assertInstanceOf(Request::class, $req);
+        $this->assertEquals($type, $req->getType());
+        $this->assertEquals($api, $req->getAPI());
+        $this->assertEquals($fialds, $req->getFields());
+    }
+}
