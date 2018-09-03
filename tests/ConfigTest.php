08c7d95a81c26fc750dd40c53e32ca767dac8dd1
Guido Falsi <mad@madpilot.net>
2018-09-03 16:23:26 +0200

    Refactor test not to use faker and without setup/teardown.

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -2,49 +2,10 @@
 
 namespace madpilot78\bottg\tests;
 
-use Faker;
 use madpilot78\bottg\Config;
 
 class ConfigTest extends \PHPUnit\Framework\TestCase
 {
-    /**
-     * @var \Faker
-     */
-    private $faker;
-
-    /**
-     * @var Bot
-     */
-    private $config;
-
-    /**
-     * Setup code.
-     *
-     * Instantiates class to be tested.
-     *
-     * @return void
-     */
-    protected function setUp()
-    {
-        parent::setUp();
-        $this->faker = Faker\Factory::create();
-        $this->config = new Config();
-    }
-
-    /**
-     * Teardown code.
-     *
-     * Frees tested object.
-     *
-     * @return void
-     */
-    protected function tearDown()
-    {
-        parent::tearDown();
-        $this->faker = null;
-        $this->config = null;
-    }
-
     /**
      * Test creating a Configuration object.
      *
@@ -52,14 +13,10 @@ class ConfigTest extends \PHPUnit\Framework\TestCase
      */
     public function testCanCreateConfigObject()
     {
-        $this->assertInstanceOf(Config::class, $this->config);
-        $c = new Config(
-            $this->faker->numberBetween($min = 0, $max = 120),
-            $this->faker->numberBetween($min = 0, $max = 120),
-            $this->faker->numberBetween($min = 0, $max = 120),
-            $this->faker->numberBetween($min = 0, $max = 120)
-        );
-        $this->assertInstanceOf(Config::class, $c);
+        $config = new Config();
+        $this->assertInstanceOf(Config::class, $config);
+        $config = new Config(90, 90, 90, 90);
+        $this->assertInstanceOf(Config::class, $config);
     }
 
     /**
@@ -92,8 +49,9 @@ class ConfigTest extends \PHPUnit\Framework\TestCase
         $setter = 'set' . $method;
         $getter = 'get' . $method;
 
-        $this->config->$setter(-10);
-        $this->assertEquals(constant('madpilot78\bottg\Config::' . $const), $this->config->$getter());
+        $config = new Config();
+        $config->$setter(-10);
+        $this->assertEquals(constant('madpilot78\bottg\Config::' . $const), $config->$getter());
     }
 
     /**
@@ -111,18 +69,19 @@ class ConfigTest extends \PHPUnit\Framework\TestCase
         $setter = 'set' . $method;
         $getter = 'get' . $method;
         $default = constant('madpilot78\bottg\Config::' . $const);
-        $to = $this->faker->unique()->numberBetween($min = 1, $max = 120);
+        $to = 42;
 
-        $this->assertEquals($default, $this->config->$getter());
+        $config = new Config();
+        $this->assertEquals($default, $config->$getter());
 
-        $this->config->$setter(0);
-        $this->assertEquals(0, $this->config->$getter());
+        $config->$setter(0);
+        $this->assertEquals(0, $config->$getter());
 
-        $this->config->$setter($to);
-        $this->assertEquals($to, $this->config->$getter());
+        $config->$setter($to);
+        $this->assertEquals($to, $config->$getter());
 
         // no argument forces default
-        $this->config->$setter();
-        $this->assertEquals($default, $this->config->$getter());
+        $config->$setter();
+        $this->assertEquals($default, $config->$getter());
     }
 }
