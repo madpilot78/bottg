555b85f582343a0462f978f67e7fdaad7521af02
Guido Falsi <mad@madpilot.net>
2018-09-20 12:55:33 +0200

    Change GetUpdates

--- src/API/GetUpdates.php
+++ src/API/GetUpdates.php
@@ -2,6 +2,7 @@
 
 namespace madpilot78\bottg\API;
 
+use InvalidArgumentException;
 use madpilot78\bottg\Config;
 use madpilot78\bottg\Http\HttpInterface;
 use madpilot78\bottg\Logger;
@@ -14,24 +15,42 @@ class GetUpdates extends Request implements RequestInterface
     /**
      * Constructor, passes correct arguments to upstream constructor.
      *
-     * @param int           $offset
+     * $args = [
+     *      (int) offset
+     * ]
+     *
+     * @param array         $args
      * @param Config        $config
      * @param Logger        $logger
      * @param HttpInterface $http
      *
+     * @throws InvalidArgumentException
+     *
      * @return void
      */
     public function __construct(
-        int $offset = null,
+        array $args,
         Config $config = null,
         Logger $logger = null,
         HttpInterface $http = null
     ) {
+        $c = count($args);
+
+        if ($c > 1) {
+            throw new InvalidArgumentException('Wrong argument count');
+        } elseif ($c == 0 || ($c == 1 && strlen($args[0]) == 0)) {
+            $args[0] = null;
+        } else {
+            if (!is_int($args[0]) && !ctype_digit($args[0])) {
+                throw new InvalidArgumentException('Offset must be numeric or null');
+            }
+        }
+
         parent::__construct(
             RequestInterface::GET,
             'getUpdates',
             [
-                'offset' => $offset
+                'offset' => $args[0]
             ],
             $config,
             $logger,
