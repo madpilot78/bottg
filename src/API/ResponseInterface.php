af0abc23de28b94ff534fdf60268a1ca9e86a811
Guido Falsi <mad@madpilot.net>
2018-10-13 17:23:30 +0200

    Add constructor to interface.

--- src/API/ResponseInterface.php
+++ src/API/ResponseInterface.php
@@ -5,6 +5,18 @@ namespace madpilot78\bottg\API;
 interface ResponseInterface
 {
     /**
+     * Constructor taking required arguments.
+     *
+     * @param string $reply
+     * @param int $code     HTTP Code, assumed 200 if omitted
+     *
+     * @throws InvalidJSONException
+     *
+     * @return void
+     */
+    public __construct(string $reply, int $code);
+
+    /**
      * Takes a json string from which to populate the object.
      *
      * @param string $reply
