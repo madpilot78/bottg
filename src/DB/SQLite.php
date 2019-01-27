d756e0cb147c3b63580c5fc6b849392b22a6da44
Guido Falsi <mad@madpilot.net>
2019-01-27 18:32:06 +0100

    Add stub SQLite class.

---
+++ src/DB/SQLite.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace madpilot78\bottg\DB;
+
+use madpilot78\bottg\Exceptions\DBException;
+use PDO;
+
+class SQLite implements DBInterface
+{
+    /**
+     * @var int DB schema version.
+     */
+    public const VERSION = 0;
+
+    /**
+     * @var PDO
+     */
+    private $dbh;
+
+    /**
+     * Creates the latest version of the DB schema
+     *
+     * @return void
+     */
+    private function createSchema(): void
+    {
+        $this->dbh->exec('CREATE TABLE dbver (version INTEGER NOT NULL UNIQUE, timestamp TEXT DEFAULT CURRENT_TIMESTAMP)');
+        $this->dbh->exec('INSERT INTO dbver (version) VALUES (' . self::VERSION . ')');
+        $this->dbh->exec('CREATE TABLE update_id (value INTEGER NOT NULL, timestamp TEXT DEFAULT CURRENT_TIMESTAMP)');
+        $this->dbh->exec('INSERT INTO update_id (value) VALUES (0)');
+    }
+
+    /**
+     * Updates to the latest version of the DB schema
+     *
+     * @param int $oldver
+     *
+     * @return void
+     */
+    private function updateSchema(int $oldver): void
+    {
+    }
+
+    /**
+     * Constructor needs to check if DB exists, check version, create or update schema.
+     *
+     * @param string $path
+     *
+     * @throws DBException
+     *
+     * @return void
+     */
+    public function __construct(string $path)
+    {
+        try {
+            $this->dbh = new PDO('sqlite:' . $path);
+            $this->dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            // Check if version table exists
+            $sth = $this->dbh->query('SELECT count(*) FROM sqlite_master WHERE type = "table" AND name = "dbver"');
+            $res = $sth->fetchColumn();
+
+            if ($res == 0) {
+                $this->createSchema();
+                return;
+            }
+
+            $sth = $this->dbh->query('SELECT MAX(ver) FROM dbver');
+            $res = $sth->fetchColumn();
+
+            if ($res < self::VERSION) {
+                $this->updateSchema($res);
+            } elseif ($res > self::VERSION) {
+                unset($this->dbh);
+                throw new DBException('Unknown DB schema version ' . $res, 99);
+            }
+        } catch (PDOException $e) {
+            throw new DBException($e->getMessage(), 1, $e);
+        }
+    }
+
+    /**
+     * Gets the update ID from the DB
+     *
+     * @return int
+     */
+    public function getUpdateID(): int
+    {
+    }
+
+    /**
+     * Saves the Update ID to the DB
+     *
+     * @param   int $id
+     *
+     * @return  void
+     */
+    public function setUpdateID(int $id): void
+    {
+    }
+}
