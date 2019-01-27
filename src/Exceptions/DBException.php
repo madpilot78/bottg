341260850717bdd946d65d721d94b0655b3b5615
Guido Falsi <mad@madpilot.net>
2019-01-27 16:42:27 +0100

    Add Exception for DB errors, will wrap PDOExecptions and other exceptions from DB drivers..

---
+++ src/Exceptions/DBException.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace madpilot78\bottg\Exceptions;
+
+class DBException extends \RuntimeException
+{
+}
---
+++ tests/Exceptions/DBExceptionTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace madpilot78\bottg\tests\Exceptions;
+
+use madpilot78\bottg\Exceptions\DBException;
+use madpilot78\bottg\tests\TestCase;
+use PDO;
+use PDOException;
+
+class DBExceptionTest extends TestCase
+{
+    /**
+     * Test throwing a DBException
+     *
+     * @expectedException        \madpilot78\bottg\Exceptions\DBException
+     * @expectedExceptionCode    0
+     *
+     * @throws DBException
+     *
+     * @return void
+     */
+    public function testThrowingDBException()
+    {
+        throw new DBException();
+    }
+
+    /**
+     * Test causing a PDO exception and rethrowing it as a DB Exception
+     *
+     * @expectedException        \madpilot78\bottg\Exceptions\DBException
+     * @expectedExceptionCode    1
+     * @expectedExceptionMessage General error: 1 no such table: test
+     *
+     * @throws DBException
+     *
+     * @return void
+     */
+    public function testPDOToDBException()
+    {
+        try {
+            $dbh = new PDO('sqlite::memory:');
+            $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $dbh->query('SELECT * FROM test');
+        } catch (PDOException $e) {
+            throw new DBException($e->getMessage(), 1, $e);
+        }
+    }
+}
